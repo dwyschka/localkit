f1ec53872d8b4a8bafff36f7f160b8945ae82331
d.wyschka <d.wyschka@sensou.de>
2025-04-12 00:05:00 +0200

    add logging to message

--- app/Http/Controllers/Petkit/DevStateReportController.php
+++ app/Http/Controllers/Petkit/DevStateReportController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Petkit;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class DevStateReportController extends Controller
 {
 
     public function __invoke(string $deviceType, Request $request)
     {
+        Log::info('Dev State REport', ['params' => $request->all()]);
         return $this->proxy($request);
 
     }
