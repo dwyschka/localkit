a6432bd639ad1a90f6b45fd8abe870935cb637a2
d.wyschka <d.wyschka@sensou.de>
2025-08-02 20:56:15 +0200

    fix build

--- config/petkit.php
+++ config/petkit.php
@@ -3,5 +3,6 @@
 return [
     'local_ip' =>  env('PETKIT_LOCAL_IP', '127.0.0.1'),
     'discovery_prefix' => env('HOMEASSISTANT_DISCOVERY_PREFIX', 'homeassistant'),
-    'bypass_auth' => env('BYPASS_AUTH', true)
+    'bypass_auth' => env('BYPASS_AUTH', true),
+    'bypass_auth_id' => env('BYPASS_AUTH_ID', 1)
 ];
