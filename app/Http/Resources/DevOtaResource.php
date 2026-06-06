bfef05d2cf89ae589520183d406b181c9282b634
d.wyschka <d.wyschka@sensou.de>
2026-06-06 22:23:22 +0200

    fix ota

--- app/Http/Resources/DevOtaResource.php
+++ app/Http/Resources/DevOtaResource.php
@@ -5,11 +5,12 @@ namespace App\Http\Resources;
 use App\Localkit\OTA;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class DevOtaResource extends PetkitHttpResource
 {
-    public static $wrap = 'result';
+    public static $wrap = null;
     /**
      * Transform the resource into an array.
      *
@@ -18,5 +19,18 @@ class DevOtaResource extends PetkitHttpResource
     public function toArray(Request $request): array
     {
         return app(OTA::class)->toOta($this->resource);
+    }
+
+    public function toResponse($request)
+    {
+        $response = response()->json(
+                $this->resolve($request),
+            200,
+            [],
+            JSON_UNESCAPED_SLASHES|JSON_PRESERVE_ZERO_FRACTION
+        );
+
+        return $response->header('Content-Type', 'application/json;charset=utf-8')
+            ->header('Content-Length', strlen($response->getContent()));
     }
 }
