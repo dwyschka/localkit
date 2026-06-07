78ea0da200cca1d9cf56a1d0f3bfe7f34a140af4
d.wyschka <d.wyschka@sensou.de>
2026-06-07 10:29:26 +0200

    Remove unneccassary log / Encode json on OTA

--- app/Http/Resources/DevOtaResource.php
+++ app/Http/Resources/DevOtaResource.php
@@ -18,23 +18,22 @@ class DevOtaResource extends PetkitHttpResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'firmwareId' => 1020,
-            'version' => '1.267',
-            'details' => [
-                [
-                    'id' => 1024,
-                    'module' => 'userbin',
-                    'version' => 2531005,
-                    'file' => [
-                        'url' => 'http://petkit-storage-firmware-prod-eu.oss-eu-central-1.aliyuncs.com/2025/7/29/deaec50a-1e6e-47cb-9c03-b78aa2eeeb63.bin',
-                        'size' => 1363200,
-                        'digest' => 'a9a2978daed4546f0e8cf40a5cb62295',
-                    ],
-                ],
+        return app(OTA::class)->toOTA($this->resource);
+    }
+
+    public function toResponse($request)
+    {
+        $response = response()->json(
+            [
+                self::$wrap => $this->resolve($request)
             ],
-        ];
-        return app(OTA::class)->toOta($this->resource);
+            200,
+            [],
+            JSON_UNESCAPED_SLASHES
+        );
+
+        return $response->header('Content-Type', 'application/json;charset=utf-8')
+            ->header('Content-Length', strlen($response->getContent()));
     }
 
 }
